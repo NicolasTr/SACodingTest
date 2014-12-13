f9c2152c78c8fe2f589bd3418b3ebfb561430cab
Nicolas Trésegnie <nicolas.tresegnie@gmail.com>
2014-12-13 09:42:58 +0000

    Set default permissions to IsAdminUser

--- backend/project/django_settings.py
+++ backend/project/django_settings.py
@@ -63,7 +63,7 @@ MIDDLEWARE_CLASSES = (
 
 REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': [
-        'rest_framework.permissions.AllowAny',
+        'rest_framework.permissions.IsAdminUser',
     ],
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework_jwt.authentication.JSONWebTokenAuthentication',
