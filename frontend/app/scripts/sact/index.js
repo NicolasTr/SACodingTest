f38bc376d4f6e77a4825d194367f5296eba7e852
Nicolas Trésegnie <nicolas.tresegnie@gmail.com>
2014-12-13 19:04:49 +0000

    Simplify states and add menu

--- frontend/app/scripts/sact/index.js
+++ frontend/app/scripts/sact/index.js
@@ -9,37 +9,22 @@ angular.module('sact').config(function($urlRouterProvider, $stateProvider) {
     $urlRouterProvider.otherwise('/');
 
     $stateProvider
-        .state('sact', {
-            templateUrl: 'scripts/sact/partials/sact.html',
-            abstract: true
-        })
         .state('reference', {
             url: '/reference/',
             templateUrl: 'scripts/sact/partials/reference.html'
         })
-        .state('sact.anonymous', {
-            templateUrl: 'scripts/sact/partials/sact.anonymous.html',
+        .state('sact', {
+            templateUrl: 'scripts/sact/partials/sact.html',
             abstract: true
         })
-        .state('sact.anonymous.login', {
-            url: '/login/',
-            templateUrl: 'scripts/sact/partials/sact.anonymous.login.html'
-        })
-        .state('sact.anonymous.register', {
-            url: '/register/',
-            templateUrl: 'scripts/sact/partials/sact.anonymous.register.html'
-        })
-        .state('sact.anonymous.logout', {
-            url: '/logout/',
-            templateUrl: 'scripts/sact/partials/sact.anonymous.login.html'
-        })
-        .state('sact.authenticated', {
-            templateUrl: 'scripts/sact/partials/sact.authenticated.html',
-            abstract: true
-        })
-        .state('sact.authenticated.canvas', {
+        .state('sact.canvas', {
             url: '/',
-            templateUrl: 'scripts/sact/partials/sact.authenticated.canvas.html',
+            templateUrl: 'scripts/sact/partials/sact.canvas.html',
+            controller: 'CanvasController'
+        })
+        .state('sact.gallery', {
+            url: '/gallery/',
+            templateUrl: 'scripts/sact/partials/sact.gallery.html',
             controller: 'CanvasController'
         });
 });
@@ -48,18 +33,9 @@ angular.module('sact').config(function (localStorageServiceProvider) {
     localStorageServiceProvider.setPrefix('sact');
 });
 
-angular.module('sact').run(function($rootScope, AuthenticationService, $state) {
-    $rootScope
-        .$on('$stateChangeStart', function(event, toState, toParams, fromState, fromParams){
-            if(!AuthenticationService.isAuthenticated()
-                && (toState.name.indexOf('sact.anonymous') != 0 && toState.name.indexOf('reference') != 0)) {
-                event.preventDefault();
-                $state.go('sact.anonymous.login');
-            }
-        });
-
-    $rootScope
-        .$on('$stateChangeError', function(event, toState, toParams, fromState, fromParams, error){
-            console.error('Error changing state: ', toState);
-        })
+angular.module('sact').run(function ($rootScope, $state) {
+    $rootScope.$state = $state;
 });
+
+// !AuthenticationService.isAuthenticated()
+// && toState.name.indexOf('sact.anonymous') != 0 && toState.name.indexOf('reference') != 0
